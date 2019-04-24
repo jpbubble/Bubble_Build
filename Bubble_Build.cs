a313fc387ae20f211de9c0640fbd4599ebf69346
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-24 19:28:53 +0200

    Builder Head

--- Bubble_Build.cs
+++ Bubble_Build.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrickyUnits;
 
 namespace Bubble {
     class Bubble_Build {
+
+        public static TGINI Project;
+        static FlagParse Flags;
+
+        static void GetAllVersions() {
+            Dirry.InitAltDrives(); // Must be done anyway, and we'll get the version in the process!
+            GINI.Hello();
+            FileList.Hello();
+            qstr.Hello();
+            QuickStream.Hello();
+            QCol.Hello();
+        }
+
+        static void Head() {            
+            QCol.Red("Bubble Builder  ");
+            QCol.Green($"Version {MKL.Newest}\n");
+            QCol.Magenta("Coded by: Jeroen P. Broks\n");
+            QCol.Yellow($"(c) {MKL.CYear(2019)} Jeroen P. Broks, Licensed under the terms of the GPL3\n\n");
+        }
+
         static void Main(string[] args) {
-            Console.WriteLine("Testing! ONE TWO THREE");
+            Flags = new FlagParse(args);
+            GetAllVersions();
+            Head();
+            QCol.OriCol();
         }
     }
 }
